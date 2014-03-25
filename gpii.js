bc7073307724d24f6d9c110eac5461a3ca07dbb7
Javier Hernández <jhernandez@emergya.com>
2014-03-25 04:16:44 +0100

    Adding a first working example of device reporting

--- gpii.js
+++ gpii.js
@@ -22,3 +22,46 @@ kettle.config.makeConfigLoader({
     nodeEnv: kettle.config.getNodeEnv("fm.ps.sr.dr.mm.os.lms.development"),
     configPath: kettle.config.getConfigPath() || "../node_modules/universal/gpii/configs"
 });
+
+/* Device Reporter
+ *
+ * This is not the place where we want to trigger this and should be moved
+ * to elsewhere.
+ *
+**/
+
+var http = require('http');
+var os = require('os');
+
+var solutionsRegistry = "";
+var installedSolutions = [];
+
+require("packagekit");
+packageKit = fluid.registerNamespace("gpii.packageKit");
+var pkgs = packageKit.get("installed;~devel");
+
+http.get("http://localhost:8081/solution/" + os.platform(), function (res) {
+
+  res.on('data', function (data) {
+    solutionsRegistry += data;
+  });
+
+  res.on('end', function() {
+    var installedSolution = null;
+    var solutions = JSON.parse(solutionsRegistry);
+    for (var solution in solutions) {
+      if ('gpii.packageKit' in solutions[solution].contexts) {
+        solutionSpec = solutions[solution].contexts['gpii.packageKit'];
+        for (var app in pkgs['data']) {
+          if (pkgs['data'][app].name == solutionSpec.name) {
+            installedSolution = {"id": solutions[solution].id};
+            installedSolutions.push(installedSolution);
+          }
+        }
+      }
+    }
+
+    console.log(installedSolutions);
+
+  });
+});
